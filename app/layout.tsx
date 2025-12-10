a0937723cb8c258d3d08284b06410f75fae12348
Rafal Sokolowski <rafalski@MacBook-Air-de-Rafal.local>
2025-12-10 15:10:12 +0100

    feat(query): Configure global QueryClient Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { QueryClient } from '@tanstack/react-query'
+import QueryProvider from "@/src/providers/QueryProvider";
 
+const queryClient = new QueryClient();
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,8 +29,10 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      > 
+      <QueryProvider>
+          {children}   
+      </QueryProvider>
       </body>
     </html>
   );
