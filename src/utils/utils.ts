ae9fc6bc79517d28cd14f731c2678e7e4db08b84
Rafal Sokolowski <rafalski@MacBook-Air-de-Rafal.local>
2026-02-05 14:10:28 +0100

    fix(utils): correct percentage scaling and code formatting

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,28 +1,29 @@
 import numeral from 'numeral';
 
 export const formatCurrency = (value: number | null | undefined, currency: string) => {
-    if (value === null || value === undefined || isNaN(value)) {
-        return 'N/A';
-    }
-    return new Intl.NumberFormat('en-US', { 
-        style: 'currency', 
-        currency: `${currency.toUpperCase()}`,
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-    }).format(value);
-}
+  if (value === null || value === undefined || isNaN(value)) {
+    return 'N/A';
+  }
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: `${currency.toUpperCase()}`,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(value);
+};
 
 export const formatPercentage = (value: number | null | undefined) => {
-    if (value === null || value === undefined) return 'N/A';
-    return value ? `${value > 0 ? '+' : ''}${(value * 100).toFixed(2)}%` : '0.00%'
-};  
+  if (value === null || value === undefined) return 'N/A';
+
+  return value !== 0 ? `${value > 0 ? '+' : ''}${value.toFixed(2)}%` : '0.00%';
+};
 
 export const formatLargeNumber = (value: number | null | undefined): string => {
-    if (value === null || value === undefined || isNaN(value)) {
-        return 'N/A';
-    }
+  if (value === null || value === undefined || isNaN(value)) {
+    return 'N/A';
+  }
 
-    const formattedValue = numeral(value).format('0.0a').toUpperCase();
-    
-    return `${formattedValue}`;
+  const formattedValue = numeral(value).format('0.0a').toUpperCase();
+
+  return `${formattedValue}`;
 };
